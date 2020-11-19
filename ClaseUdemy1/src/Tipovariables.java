df483aa3b32ac374699a038840b63357bd2b76f2
B35985 <B35985@B35985TA13W10.grupoib.local>
2020-11-18 23:51:39 -0500

    primer commit ds

---
+++ ClaseUdemy1/src/Tipovariables.java
@@ -0,0 +1,56 @@
+import javax.swing.JOptionPane;
+
+public class Tipovariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//los datos primitivos en java son : 
+		
+		//byte ,short,int ,long,float,double,boolean y char.
+		
+		
+		
+//byte 8 bits con signo puede tener valores numericos inclusive -128 a 127
+		
+		
+		String nombre = JOptionPane.showInputDialog("Ingresa tu nombre");
+		JOptionPane.showMessageDialog(null, "Hola puto " + nombre);
+		
+
+byte prueba;
+prueba=-128;
+byte prueba2; 
+prueba2=127;
+		
+		
+		
+
+
+
+		//las variables comienzan con minuscula pero puede tener mayusculas
+		
+		int primeraVariable=4;
+		int segundaVariable=6;
+		
+System.out.println(primeraVariable);		
+
+ int fdsfq1;
+ fdsfq1=5;
+ 
+primeraVariable=8;
+
+System.out.println(primeraVariable);
+
+
+
+//var inferencia de tipos en java
+
+var miVariableEntera2=2;
+var letra="prrito";
+
+System.out.println(letra);
+
+	}
+
+}
